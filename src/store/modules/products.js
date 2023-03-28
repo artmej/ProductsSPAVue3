f99859d009c8d783043939d619ec7caa4a5d01aa
Arturo Mejia <artmej@microsoft.com>
2023-03-27 20:21:15 -0600

    cambios a data

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -3,28 +3,7 @@ import axios from 'axios';
 
 const captains = console;
 
-const data = {
-  products: [
-    {
-      id: 10,
-      name: 'Strawberries',
-      description: '16oz package of fresh organic strawberries',
-      quantity: '1',
-    },
-    {
-      id: 20,
-      name: 'Sliced bread',
-      description: 'Loaf of fresh sliced wheat bread',
-      quantity: 1,
-    },
-    {
-      id: 30,
-      name: 'Apples',
-      description: 'Bag of 7 fresh McIntosh apples',
-      quantity: 1,
-    },
-  ],
-};
+
 export default {
   strict: process.env.NODE_ENV !== 'production',
   namespaced: true,
@@ -37,7 +16,7 @@ export default {
     },
   },
   actions: {
-    async getProductsAction({ commit }) {
+    async getProductsAction() {
       try {
         axios.get('./data-api/rest/Products')
              .then(function (response) {
